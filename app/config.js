78dd9bf5cd92ea15e9f404708386f75425731dd9
kronosun <67097090+kronosun@users.noreply.github.com>
2023-02-15 10:51:03 +0400

    Set config variables
    just for deployment

--- app/config.js
+++ app/config.js
@@ -1,21 +1,21 @@
 module.exports = {
 	// Twilio API keys
 	twilio: {
-		sid: "ACCOUNT-SID",
-		token: "AUTH-TOKEN",
-		appid: 'APP-ID',
-		fromNumber : "TWILIO-NUMBER",
-		welcome : "Thank you for calling.",
+		sid: "ACc20560317de178536db23ff785244bf2",
+		token: "d268c1fcd7389c6a5e9daa0d86401f16",
+		appid: 'flybase-muv0x',
+		fromNumber : "+33975183826",
+		welcome : "Merci de votre appel.",
 		hangup : false,
-		queueName: "cnacd",
-		dqueueurl:"http://yourwebsite.com/voice"
+		queueName: "b0tacd",
+		dqueueurl:"https://flybase-muv0x.herokuapp.com/voice"
 	},
 	//	Flybase settings
 	flybase: {
-		api_key: "YOUR-API-KEY",
-		app_name: "YOUR-FLYBASE-APP"
+		api_key: "948e5b18-290b-434f-95f1-9f2108e2b679",
+		app_name: "muv0x"
 	},
 	//	Username and password for admin section.
-	un: 'admin',
-	pw: 'password'
+	un: 'voxadmin',
+	pw: 'Muv0x@31337!'
 };
